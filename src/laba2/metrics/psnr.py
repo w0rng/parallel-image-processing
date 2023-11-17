ffdfb012de58adc7a3b63158b940f17cdd6b53b6
w0rng <work@w0rng.ru>
2023-11-17 12:35:46 +0700

    drop old files

--- src/laba2/metrics/psnr.py
+++
@@ -1,7 +0,0 @@
-from image import Image
-from .mse import mse
-from math import log10
-
-
-def psnr(a: Image, b: Image) -> float:
-    return 10 * log10((255 ** 2) / mse(a, b))
--- src/laba2/utils/clip.py
+++
@@ -1,6 +0,0 @@
-def clip(num: float, min: float, max: float) -> float:
-    if num < min:
-        return min
-    if num > max:
-        return max
-    return num
