477e7de702ed9d78caa6e7b4099219ceb002b56f
w0rng <work@w0rng.ru>
2023-12-24 13:59:45 +0700

    добавил инпутбокс

--- src/layouts/hough_transform_linear.py
+++ src/layouts/hough_transform_linear.py
@@ -4,14 +4,16 @@ from src.algorithms.hough_transform_linear import hough_transform_linear
 
 def make_layout(window) -> QBoxLayout:
     make_button = QPushButton("Сделать")
+    count_line = QLineEdit(text="10")
 
     def make_button_clicked():
-        hough_transform_linear(window.current_image).show()
+        hough_transform_linear(window.current_image, int(count_line.text())).show()
 
     make_button.clicked.connect(make_button_clicked)
 
     color_models_layout = QHBoxLayout()
     color_models_layout.addWidget(QLabel('Хаф ищет линии'))
+    color_models_layout.addWidget(count_line)
 
     color_models_layout.addWidget(make_button)
 
