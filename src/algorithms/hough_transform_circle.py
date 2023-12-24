2cfeefc3dd24f48cdb798db3c55d350e0bf3cbd0
Artem Rylov <tema2707@icloud.com>
2023-12-24 21:33:44 +0700

    выглядит как хуйня (расспаралеллил хоугх)

--- src/algorithms/hough_transform_circle.py
+++ src/algorithms/hough_transform_circle.py
@@ -6,33 +6,52 @@ import numpy as np
 
 from algorithms.contours.sobel import sobel_method
 from image import Image
+from utils import Pool
+
+
+def hough_circle_row(image_matrix: [[[int]]], y: int, width: int, height: int, min_radius: int, max_radius: int):
+    circles = defaultdict(lambda: 0)
+
+    for x in range(width):
+        # Если текущий пиксель является граничным (принадлежит объекту)
+        if image_matrix[y][x][0] != 255:
+            continue
+        # Проход по возможным радиусам окружности
+        for r in range(min_radius, max_radius + 1):
+             for angle in range(0, 360):
+                # Вычисление координат центра окружности
+                a = x - r * math.cos(math.radians(angle))
+                b = y - r * math.sin(math.radians(angle))
+                if 0 <= round(a) < width and 0 <= round(b) < height:
+                    circles[(round(a), round(b), r)] += 1
+
+    return dict(circles)
+
+
+def _tmp_hough_circle_row(args):
+    return hough_circle_row(*args)
 
 
 def hough_circle(image_matrix, min_radius, max_radius):
     height = len(image_matrix)
     width = len(image_matrix[0])
 
-    # Создание пустого аккумулятора (accumulator)
-    circles = defaultdict(lambda: 0)
-    coss = {angle: math.cos(math.radians(angle)) for angle in range(0, 360)}
-    sins = {angle: math.sin(math.radians(angle)) for angle in range(0, 360)}
+    circles_dicts = []
+    for count in range(1, 5):
+        with Pool("hough_transform_circle", count) as pool:
+            circles_dicts = pool.map(
+                _tmp_hough_circle_row,
+                [(image_matrix, y, width, height, min_radius, max_radius)
+                 for y in range(height)]
+            )
 
-    # Проход по всем пикселям изображения
-    for y in range(height):
-        for x in range(width):
-            # Если текущий пиксель является граничным (принадлежит объекту)
-            if image_matrix[y][x][0] != 255:
-                continue
-            # Проход по возможным радиусам окружности
-            for r in range(min_radius, max_radius + 1):
-                for angle in range(0, 360):
-                    # Вычисление координат центра окружности
-                    a = x - r * coss[angle]
-                    b = y - r * sins[angle]
-                    if 0 <= round(a) < width and 0 <= round(b) < height:
-                        circles[(round(a), round(b), r)] += 1
+    result = defaultdict(lambda: 0)
 
-    return circles
+    for circle_dict in circles_dicts:
+        for key, value in circle_dict.items():
+            result[key] += value
+
+    return result
 
 
 def draw_circles(image, circles):
