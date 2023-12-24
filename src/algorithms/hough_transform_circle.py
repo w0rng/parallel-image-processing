19001808e8253c7871f776e6b48552c5a98da06b
w0rng <work@w0rng.ru>
2023-12-24 20:57:24 +0700

    хаф кружочки пук

--- src/algorithms/hough_transform_circle.py
+++ src/algorithms/hough_transform_circle.py
@@ -1,6 +1,9 @@
 import math
+from collections import defaultdict
+from time import time
 
 import numpy as np
+from joblib import Parallel, delayed
 
 from algorithms.contours.sobel import sobel_method
 from image import Image
@@ -11,29 +14,24 @@ def hough_circle(image_matrix, min_radius, max_radius):
     width = len(image_matrix[0])
 
     # Создание пустого аккумулятора (accumulator)
-    accumulator = [[[0 for _ in range(max_radius - min_radius + 1)] for _ in range(width)] for _ in range(height)]
+    circles = defaultdict(lambda: 0)
+    coss = {angle: math.cos(math.radians(angle)) for angle in range(0, 360)}
+    sins = {angle: math.sin(math.radians(angle)) for angle in range(0, 360)}
 
     # Проход по всем пикселям изображения
     for y in range(height):
         for x in range(width):
             # Если текущий пиксель является граничным (принадлежит объекту)
-            if image_matrix[y][x] != (
-                    0, 0, 0):  # Проверка на черный цвет, можно изменить в зависимости от формата цветов
-                # Проход по возможным радиусам окружности
-                for r in range(min_radius, max_radius + 1):
-                    for angle in range(0, 360):
-                        # Вычисление координат центра окружности
-                        a = x - r * math.cos(math.radians(angle))
-                        b = y - r * math.sin(math.radians(angle))
-                        if 0 <= round(a) < width and 0 <= round(b) < height:
-                            accumulator[round(b)][round(a)][r - min_radius] += 1
-
-    # Поиск окружностей с числом голосов выше порога
-    circles = {}
-    for y in range(height):
-        for x in range(width):
-            for r in range(max_radius - min_radius + 1):
-                circles[(x, y, r + min_radius)] = accumulator[y][x][r]
+            if image_matrix[y][x][0] != 255:
+                continue
+            # Проход по возможным радиусам окружности
+            for r in range(min_radius, max_radius + 1):
+                for angle in range(0, 360):
+                    # Вычисление координат центра окружности
+                    a = x - r * coss[angle]
+                    b = y - r * sins[angle]
+                    if 0 <= round(a) < width and 0 <= round(b) < height:
+                        circles[(round(a), round(b), r)] += 1
 
     return circles
 
@@ -47,6 +45,7 @@ def draw_circles(image, circles):
                 image[y1][x1] = (255, 0, 0)
     return image
 
+
 def get_top_circles(circles, count):
     top_value = sorted(circles.values(), reverse=True)[:count][-1]
     result = []
@@ -58,7 +57,13 @@ def get_top_circles(circles, count):
 
 def hough_transform_circle(image: Image, count: int):
     contur = sobel_method(image, 150, 3, 10)
+    start = time()
     circles = hough_circle(contur.pixels, min(image.size) // 10, min(image.size) // 2)
+    print(f"Time: {time() - start}")
     result = get_top_circles(circles, count)
     img = draw_circles(image.pixels, result)
     return Image(pixels=img)
+
+
+def run_hough_transform_with_joblib(image, count):
+    return Parallel(n_jobs=-1)(delayed(hough_transform_circle)(image, count) for _ in range(1))[0]
