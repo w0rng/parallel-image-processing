5a52fd29282a5a9850640da7a3a0c222391cbc5a
Artem Rylov <tema2707@icloud.com>
2023-12-25 14:45:39 +0700

    пахней хуйней

--- src/algorithms/hough_transform_linear.py
+++ src/algorithms/hough_transform_linear.py
@@ -4,6 +4,25 @@ from copy import deepcopy
 
 from algorithms.contours.sobel import sobel_method
 from image import Image
+from utils import Pool
+
+
+def hough_transform_row(pixels, y: int, width: int, theta_max: int) -> dict:
+    accumulator = defaultdict(lambda: 0)
+
+    for x in range(width):
+        if pixels[y][x][0] != 255:  # Если текущий пиксель не является фоном (не черный)
+            continue
+        for theta in range(theta_max):
+            # Преобразование координат изображения в параметры прямой (ρ, theta)
+            rho = int(x * math.cos(math.radians(theta)) + y * math.sin(math.radians(theta)))
+            accumulator[(rho, theta)] += 1  # Увеличиваем значение в аккумуляторе
+
+    return dict(accumulator)
+
+
+def _tmp_hough_circle_row(args):
+    return hough_transform_row(*args)
 
 
 def hough_transform(image):
@@ -11,21 +30,21 @@ def hough_transform(image):
     width = len(image[0])
     theta_max = 180  # Максимальное значение угла theta (в градусах)
 
-    # Инициализация массива аккумуляторов
-    accumulator = defaultdict(lambda: 0)
-    coss = {theta: math.cos(math.radians(theta)) for theta in range(theta_max)}
-    sins = {theta: math.sin(math.radians(theta)) for theta in range(theta_max)}
+    dicts = []
+    for count in range(1, 5):
+        with Pool("hough_transform_linear", count) as pool:
+            dicts = pool.map(
+                _tmp_hough_circle_row,
+                [(image, y, width, theta_max) for y in range(height)]
+            )
 
-    for y in range(height):
-        for x in range(width):
-            if image[y][x][0] != 255:  # Если текущий пиксель не является фоном (не черный)
-                continue
-            for theta in range(theta_max):
-                # Преобразование координат изображения в параметры прямой (ρ, theta)
-                rho = int(x * coss[theta] + y * sins[theta])
-                accumulator[(rho, theta)] += 1  # Увеличиваем значение в аккумуляторе
+    result = defaultdict(lambda: 0)
 
-    return accumulator
+    for d in dicts:
+        for key, value in d.items():
+            result[key] += value
+
+    return result
 
 
 def draw_detected_lines(image, detected_lines):
